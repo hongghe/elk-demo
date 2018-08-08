9be95b5a83c8d99473a6768d5115078211367931
hongghe <364930855@qq.com>
2018-08-08 23:34:42 +0800

    elasticsearch

--- src/main/java/com/hongghe/elkdemo/ElkDemoApplication.java
+++ src/main/java/com/hongghe/elkdemo/ElkDemoApplication.java
@@ -3,6 +3,11 @@ package com.hongghe.elkdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The startup of project.
+ *
+ * @author hongghe 08/08/2018
+ */
 @SpringBootApplication
 public class ElkDemoApplication {
 
